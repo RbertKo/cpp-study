6b93c62713dfaea4a8a89e81d48f7198189874be
RbertKo <myeongsku@gmail.com>
2021-03-22 03:00:57 +0900

    Add goorm-bit2

--- hello-world/hello-world/hello-world.cpp
+++ hello-world/hello-world/hello-world.cpp
@@ -1,11 +1,16 @@
 ﻿// hello-world.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
-
+#include <string>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string name, title;
+
+    std::cin >> name;
+    std::cin >> title;
+
+    std::cout << name << "'s favourite movie is " << title;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
