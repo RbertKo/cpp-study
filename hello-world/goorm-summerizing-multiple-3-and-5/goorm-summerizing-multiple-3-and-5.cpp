0a55b95952e1ee84250a2177feb1450d75abcf6a
RbertKo <myeongsku@gmail.com>
2021-03-22 03:17:26 +0900

    Create groom-summerizing-multiple-3-and-5

---
+++ hello-world/goorm-summerizing-multiple-3-and-5/goorm-summerizing-multiple-3-and-5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int input;
+	unsigned int count = 0;
+	cin >> input;
+
+	for (int i = 1; i <= input; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			count += i;
+		}
+	}
+
+	cout << count << endl;
+	return 0;
+}
