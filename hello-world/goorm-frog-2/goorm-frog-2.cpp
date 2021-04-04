798d803012af267d25f473cce5b7c2d6d37e41cb
RbertKo <myeongsku@gmail.com>
2021-04-05 00:49:13 +0900

    save problem

--- hello-world/goorm-frog-2/goorm-frog-2.cpp
+++ hello-world/goorm-frog-2/goorm-frog-2.cpp
@@ -1,20 +1,26 @@
-﻿// goorm-frog-2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int length, total = 0, cur_val = 0, cur_power = 0;
+	cin >> length;
 
-#include <iostream>
+	for (int i = 0; i < length; i++) {
+		int val, required_p;
+		cin >> val;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+		required_p = val - cur_val;
+
+		if (cur_power < required_p) {
+			int need = required_p - cur_power;
+			total += need;
+			cur_power += need;
+		}
+
+		cur_val = val;
+		cur_power -= 1;
+	}
+
+	cout << total << endl;
+
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
