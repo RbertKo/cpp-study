20a34f52058c56170403cbe684a568a6d4070705
RbertKo <myeongsku@gmail.com>
2021-04-20 23:48:43 +0900

    Get input

--- hello-world/baekjoon-14889/baekjoon-14889.cpp
+++ hello-world/baekjoon-14889/baekjoon-14889.cpp
@@ -1,6 +1,16 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+
 }
