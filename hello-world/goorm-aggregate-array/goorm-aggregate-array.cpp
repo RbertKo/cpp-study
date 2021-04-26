ef26e2c811d6a0cc97a59bb6b14ee597c3ca1b20
RbertKo <myeongsku@gmail.com>
2021-04-26 23:28:18 +0900

    finish goorm

---
+++ hello-world/goorm-aggregate-array/goorm-aggregate-array.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+	int a_len, b_len;
+
+	cin >> a_len;
+	cin >> b_len;
+
+	int* input = new int[a_len + b_len];
+
+
+	for (int i = 0; i < a_len + b_len; i++) {
+		cin >> input[i];
+	}
+
+	sort(input, input + a_len + b_len);
+
+	for (int i = 0; i < a_len + b_len; i++) {
+		cout << input[i] << ' ';
+	}
+
+	delete[] input;
+
+	return 0;
+}
